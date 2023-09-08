68041b2df9c44b618a7248c37b9ac51b07cf5b4a
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2023-09-08 17:04:51 +0100

    feat: sums

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,13 +1,39 @@
 // components/Summary.js
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../AppContext';
 
-const Summary = ({ formData }) => {
+const Summary = () => {
+  const { selectedAddOns, selectedPlan, isYearly, addOns, planPrices } = useContext(AppContext);
+
+
+  const totalAddOnPrice = selectedAddOns.reduce((total, id) => {
+    const selectedAddOn = addOns.find((addOn) => addOn.id === id);
+    if (selectedAddOn) {
+      return total + (isYearly? selectedAddOn.yearlyPrice: selectedAddOn.monthlyPrice);
+    }
+    return total;
+  }, 0);
+
+
+  const totalWithPlanPrice = (isYearly
+    ? selectedPlan.yearlyPrice : selectedPlan.monthlyPrice) + totalAddOnPrice;
+
   return (
     <div className="summary">
-      <h2>Summary</h2>
-      <p>Name: {formData.name}</p>
-      <p>Email: {formData.email}</p>
-      {/* Display other form data */}
+      <h2>Total</h2>
+      <h3>{selectedPlan.name} ({isYearly ?`yearly`: `monthly`})</h3>
+      <p>Price: {isYearly ? `$${selectedPlan.yearlyPrice}/yr` : `$${selectedPlan.monthlyPrice}/month`}</p>
+        <ul>
+          {selectedAddOns.map((id) => {
+            const selectedAddOn = addOns.find((addOn) => addOn.id === id);
+            return (
+              <li key={id}>
+                {selectedAddOn.title} - ${isYearly ? `${selectedAddOn.yearlyPrice}/yr` : `${selectedAddOn.monthlyPrice}/month`}
+              </li>
+            );
+          })}
+          <li>total - ${totalWithPlanPrice}</li>
+        </ul>
     </div>
   );
 };
